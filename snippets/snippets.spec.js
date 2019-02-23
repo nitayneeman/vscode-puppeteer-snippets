a2b3b462152d6201386aa17257f561990de594e0
nitayneeman <nitayneeman@users.noreply.github.com>
2019-02-23 13:33:03 +0200

    Added test

--- snippets/snippets.spec.js
+++ snippets/snippets.spec.js
@@ -1,3 +1,9 @@
 const snippets = require('./snippets.json');
 
-describe('Snippets', () => {});
+describe('Snippets', () => {
+  it('should not be empty', () => {
+    const keys = Object.keys(snippets);
+
+    expect(keys.length).toBeGreaterThan(0);
+  });
+});
