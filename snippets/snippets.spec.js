909e05e93ec4893b4bac8b351c09cd755679ba90
nitayneeman <nitayneeman@users.noreply.github.com>
2019-02-23 13:56:15 +0200

    Added tests

--- snippets/snippets.spec.js
+++ snippets/snippets.spec.js
@@ -1,9 +1,32 @@
 const snippets = require('./snippets.json');
 
 describe('Snippets', () => {
-  it('should not be empty', () => {
-    const keys = Object.keys(snippets);
+  const keys = Object.keys(snippets);
 
+  it('should not be empty', () => {
     expect(keys.length).toBeGreaterThan(0);
+  });
+
+  describe.each(keys)('%s', key => {
+    it('should have a prefix', () => {
+      const { prefix } = snippets[key];
+
+      expect(prefix).toBeDefined();
+      expect(prefix).not.toEqual('');
+    });
+
+    it('should have a body', () => {
+      const { body } = snippets[key];
+
+      expect(body).toBeDefined();
+      expect(body).not.toEqual('');
+    });
+
+    it('should have a description', () => {
+      const { description } = snippets[key];
+
+      expect(description).toBeDefined();
+      expect(description).not.toEqual('');
+    });
   });
 });
